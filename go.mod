f08fb19cb3e59fe9430191fb523ba39cfb6cf1ce
bryacka <bryacka@gmail.com>
2026-01-22 21:11:49 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bryack/lgwt_app
+
+go 1.25.5
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/bryack/lgwt_app/server"
+)
+
+func main() {
+	server := &server.PlayerServer{
+		Store: server.NewInMemoryPlayerStore(),
+	}
+	log.Fatal(http.ListenAndServe(":5000", server))
+}
