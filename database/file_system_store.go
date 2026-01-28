fa2b53854c75a4d58a4a558408cd1b09f942e170
bryacka <bryacka@gmail.com>
2026-01-28 10:55:43 +0300

    implement GetPlayerScore

--- database/file_system_store.go
+++ database/file_system_store.go
@@ -7,10 +7,23 @@ import (
 )
 
 type FileSystemPlayerStore struct {
-	database io.Reader
+	database io.ReadSeeker
 }
 
 func (f *FileSystemPlayerStore) GetLeague() []domain.Player {
+	f.database.Seek(0, io.SeekStart)
 	league, _ := NewLeague(f.database)
 	return league
+}
+
+func (f *FileSystemPlayerStore) GetPlayerScore(name string) int {
+	var wins int
+
+	for _, v := range f.GetLeague() {
+		if v.Name == name {
+			wins = v.Wins
+			break
+		}
+	}
+	return wins
 }
--- database/file_system_store_test.go
+++ database/file_system_store_test.go
@@ -9,20 +9,28 @@ import (
 )
 
 func TestFileSystemStore(t *testing.T) {
-
-	t.Run("league from a reader", func(t *testing.T) {
-		database := strings.NewReader(`[
+	database := strings.NewReader(`[
 			{"Name": "Cleo", "Wins": 10},
 			{"Name": "Chris", "Wins": 33}]`)
 
-		store := FileSystemPlayerStore{database: database}
+	store := FileSystemPlayerStore{database: database}
 
+	t.Run("league from a reader", func(t *testing.T) {
 		got := store.GetLeague()
 
 		want := []domain.Player{
 			{"Cleo", 10},
 			{"Chris", 33},
 		}
+
+		assert.Equal(t, want, got)
+		got = store.GetLeague()
+		assert.Equal(t, want, got)
+	})
+
+	t.Run("get player score", func(t *testing.T) {
+		got := store.GetPlayerScore("Chris")
+		want := 33
 
 		assert.Equal(t, want, got)
 	})
