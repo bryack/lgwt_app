a05285ac2882e6c33f9b6c4d370303b95b0abc9f
bryacka <bryacka@gmail.com>
2026-01-30 17:51:21 +0300

    - expand PlayPoker: iterating over an array of blinds and calling the scheduler on an increasing blindTime

--- adapters/cli/CLI_test.go
+++ adapters/cli/CLI_test.go
@@ -1,6 +1,7 @@
 package cli_test
 
 import (
+	"fmt"
 	"strings"
 	"testing"
 	"time"
@@ -49,16 +50,49 @@ func TestCLI(t *testing.T) {
 		want := "Cleo"
 		assert.Equal(t, want, got)
 	})
+
 	t.Run("it schedules printing of blind values", func(t *testing.T) {
-		in := strings.NewReader("Chris wins\n")
+		in := strings.NewReader("Cleo wins\n")
 		playerStore := &testhelpers.StubPlayerStore{}
 		blindAlerter := &SpyBlindAlerter{}
-
 		c := cli.NewCLI(playerStore, in, blindAlerter)
 		c.PlayPoker()
 
-		if len(blindAlerter.alerts) != 1 {
-			t.Fatal("expected a blind alert to be scheduled")
+		tests := []struct {
+			expectedScheduleTime time.Duration
+			expectedAmount       int
+		}{
+			{0 * time.Second, 100},
+			{10 * time.Minute, 200},
+			{20 * time.Minute, 300},
+			{30 * time.Minute, 400},
+			{40 * time.Minute, 500},
+			{50 * time.Minute, 600},
+			{60 * time.Minute, 800},
+			{70 * time.Minute, 1000},
+			{80 * time.Minute, 2000},
+			{90 * time.Minute, 4000},
+			{100 * time.Minute, 8000},
+		}
+
+		for i, tt := range tests {
+			t.Run(fmt.Sprintf("%d scheduled for %v", tt.expectedAmount, tt.expectedScheduleTime), func(t *testing.T) {
+				if len(blindAlerter.alerts) <= 1 {
+					t.Fatalf("alert %d was not scheduled %v", i, blindAlerter.alerts)
+				}
+
+				alert := blindAlerter.alerts[i]
+
+				amountGot := alert.amount
+				if amountGot != tt.expectedAmount {
+					t.Errorf("got amount %d, want %d", amountGot, tt.expectedAmount)
+				}
+
+				gotScheduledTime := alert.scheduledAt
+				if gotScheduledTime != tt.expectedScheduleTime {
+					t.Errorf("got scheduled time of %v, want %v", gotScheduledTime, tt.expectedScheduleTime)
+				}
+			})
 		}
 	})
 }
